a746f43bb537375294328d9e66f9e08d832d8cf4
venticx <venticx@gmail.com>
2025-06-29 15:25:30 +0200

    added sharp memory display driver library to flake and patched it to remove example build

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
     pico-sdk = pkgs.fetchgit {
@@ -13,6 +13,27 @@
       rev = "2.1.1";
       sha256 = "8ru1uGjs11S2yQ+aRAvzU53K8mreZ+CC3H+ijfctuqg=";
       fetchSubmodules = true;
+    };
+
+    sharp-display-patched = pkgs.stdenv.mkDerivation {
+      pname = "sharp-memory-display-driver";
+      version = "a29788c";
+
+      src = pkgs.fetchgit {
+        url = "https://github.com/Piorkos/sharp-memory-display-driver.git";
+        rev = "118c50a6c648c41989b22b29922b3bf8ba29788c";
+        sha256 = "sha256-j0JTY+fDRmvZ/m7Lc4Sw3XgDiveaqjE/BuSOSuB2EGc=";
+      };
+
+      patches = [ ./patches/remove_example_auto_set_url.patch ];
+
+      dontBuild = true;
+      dontConfigure = true;
+
+      installPhase = ''
+        mkdir -p $out
+        cp -r . $out/
+      '';
     };
   in {
     packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
@@ -32,7 +53,7 @@
         echo "Configuration..."
         mkdir build
         cd build
-        cmake .. -DPICO_SDK_PATH=${pico-sdk}
+        cmake .. -DPICO_SDK_PATH=${pico-sdk} -DDISPLAY_DRIVER_PATH=${sharp-display-patched}
       '';
 
       buildPhase = ''
