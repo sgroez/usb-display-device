fbf749c5cea167f08789da200726be5019c2f203
venticx <venticx@gmail.com>
2025-07-15 18:00:49 +0200

    changed usb display driver from kernel to userspace driver to work with evdi kernel driver

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
   outputs = { self, nixpkgs, }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
     kernel = pkgs.linuxPackages.kernel;
+    lib = pkgs.lib;
 
     sharedNativeBuildInputs = [
       pkgs.cmake
@@ -22,6 +23,43 @@
       rev = "2.1.1";
       sha256 = "8ru1uGjs11S2yQ+aRAvzU53K8mreZ+CC3H+ijfctuqg=";
       fetchSubmodules = true;
+    };
+
+    libevdi = pkgs.stdenv.mkDerivation rec {
+      pname = "libevdi";
+      version = "1.14.10";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "DisplayLink";
+        repo = "evdi";
+        rev = "v1.14.10";
+        sha256 = "sha256-xB3AHg9t/X8vw5p7ohFQ+WuMjb1P8DAP3pROiwWkVPs=";
+      };
+
+      nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config pkgs.gcc pkgs.libdrm ];
+
+      dontConfigure = true;
+
+      cmakeFlags = [
+        "-DEVKMD_DISABLE_TESTS=ON"
+        "-DEVKMD_BUILD_MODULE=OFF"
+      ];
+
+      # We only want libevdi, not kernel module
+      sourceRoot = "source/library";
+
+      installPhase = ''
+        mkdir -p $out/include/libevdi
+        mkdir -p $out/lib
+        cp evdi_lib.h $out/include/libevdi/
+        cp libevdi.so* $out/lib/
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Userspace library for evdi (DisplayLink virtual display)";
+        license = licenses.gpl2Plus;
+        maintainers = with maintainers; [ ];
+      };
     };
   in {
     packages.x86_64-linux = {
@@ -134,19 +172,24 @@
       usb_display_driver = pkgs.stdenv.mkDerivation {
         name = "usb_display_driver";
         src = ./src/usb_display_driver;
-        nativeBuildInputs = sharedNativeBuildInputs;
+        nativeBuildInputs = sharedNativeBuildInputs ++ [
+          pkgs.libusb1
+          libevdi
+        ];
         dontConfigure = true;
 
         buildPhase = ''
-          echo "Building..."
-          make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build M=$(pwd) modules
+          gcc -o usb_display_driver usb_display_driver.c \
+          -I${libevdi}/include \
+          -I${libevdi}/lib -levdi \
+          -lusb-1.0
         '';
 
         installPhase = ''
           echo "Installation..."
           mkdir -p $out
-          cp usb_display_driver.ko $out/
-          echo "Loadable kernel module copied to $out/usb_display_driver.ko"
+          cp usb_display_driver $out/
+          echo "Loadable kernel module copied to $out/usb_display_driver"
         '';
       };
 
--- src/usb_display_driver/usb_display_driver.c
+++ src/usb_display_driver/usb_display_driver.c
@@ -1,148 +1,97 @@
-// SPDX-License-Identifier: GPL-2.0
-#include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/of_device.h>
-#include <linux/drm/drm_drv.h>
-#include <linux/drm/drm_device.h>
-#include <linux/drm/drm_file.h>
-#include <linux/drm/drm_modeset_helper.h>
-#include <linux/drm/drm_simple_kms_helper.h>
-#include <linux/drm/drm_fbdev_generic.h>
-#include <drm/drm_probe_helper.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libusb-1.0/libusb.h>
+#include "libevdi/evdi_lib.h"  // Make sure to have libevdi headers and link
 
-#define DRIVER_NAME     "simpledrm"
-#define DRIVER_DESC     "Simple DRM driver"
-#define DRIVER_DATE     "20250714"
-#define DRIVER_MAJOR    1
-#define DRIVER_MINOR    0
+#define VENDOR_ID  0x1234  // USB device VID
+#define PRODUCT_ID 0x5678  // USB device PID
+#define INTERFACE_NUM 0
+#define ENDPOINT_OUT  0x01
 
-struct simple_drm_device {
-    struct drm_device drm;
-    struct drm_simple_display_pipe pipe;
-    struct drm_connector connector;
-};
+int main() {
+    libusb_device_handle *usb_handle = NULL;
+    int r;
 
-static const uint32_t formats[] = {
-    DRM_FORMAT_XRGB8888,
-};
+    // --- Initialize libusb ---
+    r = libusb_init(NULL);
+    if (r < 0) {
+        fprintf(stderr, "libusb init error: %s\n", libusb_error_name(r));
+        return 1;
+    }
 
-static void simple_drm_pipe_enable(struct drm_simple_display_pipe *pipe,
-                                   struct drm_crtc_state *crtc_state,
-                                   struct drm_plane_state *plane_state) {
-    // Normally where you'd enable the display - NOP for now
-}
+    // Open USB device
+    usb_handle = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
+    if (!usb_handle) {
+        fprintf(stderr, "USB device not found\n");
+        libusb_exit(NULL);
+        return 1;
+    }
 
-static void simple_drm_pipe_disable(struct drm_simple_display_pipe *pipe) {
-    // Disable logic - NOP for now
-}
+    r = libusb_claim_interface(usb_handle, INTERFACE_NUM);
+    if (r) {
+        fprintf(stderr, "Cannot claim USB interface: %s\n", libusb_error_name(r));
+        libusb_close(usb_handle);
+        libusb_exit(NULL);
+        return 1;
+    }
 
-static int simple_drm_pipe_check(struct drm_simple_display_pipe *pipe,
-                                 struct drm_plane_state *plane_state,
-                                 struct drm_crtc_state *crtc_state) {
+    // --- Initialize evdi ---
+    evdi_device_context *evdi_ctx = evdi_open(0);
+    if (!evdi_ctx) {
+        fprintf(stderr, "Failed to open evdi device\n");
+        libusb_release_interface(usb_handle, INTERFACE_NUM);
+        libusb_close(usb_handle);
+        libusb_exit(NULL);
+        return 1;
+    }
+
+    evdi_connect(evdi_ctx);
+    evdi_enable(evdi_ctx);
+
+    while (1) {
+        void *framebuffer;
+        int stride;
+        int width = evdi_ctx->mode_set.width;
+        int height = evdi_ctx->mode_set.height;
+
+        // Grab the latest framebuffer data with a 1000 ms timeout
+        r = evdi_grab_pixbuf(evdi_ctx, &framebuffer, &stride, 1000);
+        if (r != 0) {
+            fprintf(stderr, "Failed to grab evdi framebuffer: %d\n", r);
+            continue;
+        }
+
+        // Here you have raw ARGB framebuffer data at 'framebuffer'
+        // For simplicity, send raw data directly over USB bulk endpoint
+
+        int total_size = stride * height;
+        int offset = 0;
+        int chunk_size = 4096;
+        int actual_len;
+
+        while (offset < total_size) {
+            int send_len = (total_size - offset) > chunk_size ? chunk_size : (total_size - offset);
+            r = libusb_bulk_transfer(usb_handle, ENDPOINT_OUT,
+                                     (unsigned char*)framebuffer + offset,
+                                     send_len, &actual_len, 1000);
+            if (r != 0 || actual_len != send_len) {
+                fprintf(stderr, "USB transfer error: %s\n", libusb_error_name(r));
+                break;
+            }
+            offset += send_len;
+        }
+
+        // Optional: add delay or synchronization here
+    }
+
+    evdi_disable(evdi_ctx);
+    evdi_disconnect(evdi_ctx);
+    evdi_close(evdi_ctx);
+
+    libusb_release_interface(usb_handle, INTERFACE_NUM);
+    libusb_close(usb_handle);
+    libusb_exit(NULL);
+
     return 0;
 }
-
-static struct drm_simple_display_pipe_funcs pipe_funcs = {
-    .enable     = simple_drm_pipe_enable,
-    .disable    = simple_drm_pipe_disable,
-    .check      = simple_drm_pipe_check,
-};
-
-DEFINE_DRM_GEM_CMA_FOPS(simple_drm_fops);
-
-static struct drm_driver simple_drm_driver = {
-    .driver_features    = DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC,
-    .fops               = &simple_drm_fops,
-    .name               = DRIVER_NAME,
-    .desc               = DRIVER_DESC,
-    .date               = DRIVER_DATE,
-    .major              = DRIVER_MAJOR,
-    .minor              = DRIVER_MINOR,
-};
-
-static const struct drm_mode_modeinfo default_mode = {
-    .name = "1024x768",
-    .clock = 65000,
-    .hdisplay = 1024,
-    .hsync_start = 1048,
-    .hsync_end = 1184,
-    .htotal = 1344,
-    .vdisplay = 768,
-    .vsync_start = 771,
-    .vsync_end = 777,
-    .vtotal = 806,
-    .type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED,
-};
-
-static int simple_drm_bind(struct platform_device *pdev)
-{
-    struct simple_drm_device *sdev;
-    struct drm_device *drm;
-    int ret;
-
-    sdev = devm_kzalloc(&pdev->dev, sizeof(*sdev), GFP_KERNEL);
-    if (!sdev)
-        return -ENOMEM;
-
-    drm = &sdev->drm;
-
-    ret = drm_dev_init(drm, &simple_drm_driver, &pdev->dev);
-    if (ret)
-        return ret;
-
-    platform_set_drvdata(pdev, drm);
-
-    ret = drm_simple_display_pipe_init(drm, &sdev->pipe, &pipe_funcs,
-                                       formats, ARRAY_SIZE(formats),
-                                       NULL, &sdev->connector);
-    if (ret)
-        goto err_drm;
-
-    drm_mode_connector_attach_encoder(&sdev->connector, sdev->pipe.encoder);
-
-    sdev->connector.display_info.width_mm = 270;
-    sdev->connector.display_info.height_mm = 203;
-
-    drm_connector_init(drm, &sdev->connector,
-                       &drm_connector_funcs,
-                       DRM_MODE_CONNECTOR_VIRTUAL);
-    drm_connector_helper_add(&sdev->connector, &drm_simple_connector_helper_funcs);
-
-    drm_connector_register(&sdev->connector);
-
-    drm_mode_config_init(drm);
-    drm_mode_config_reset(drm);
-
-    drm_connector_set_panel_orientation(&sdev->connector, DRM_MODE_PANEL_ORIENTATION_NORMAL);
-    drm_mode_connector_update_edid_property(&sdev->connector, NULL);
-
-    drm_fbdev_generic_setup(drm, 32);
-
-    return drm_dev_register(drm, 0);
-
-err_drm:
-    drm_dev_put(drm);
-    return ret;
-}
-
-static void simple_drm_unbind(struct platform_device *pdev)
-{
-    struct drm_device *drm = platform_get_drvdata(pdev);
-    drm_dev_unregister(drm);
-    drm_dev_put(drm);
-}
-
-static struct platform_driver simple_drm_platform_driver = {
-    .probe  = simple_drm_bind,
-    .remove_new = simple_drm_unbind,
-    .driver = {
-        .name = DRIVER_NAME,
-    },
-};
-
-module_platform_driver(simple_drm_platform_driver);
-
-MODULE_DESCRIPTION("Simple DRM Display Driver");
-MODULE_AUTHOR("ChatGPT");
-MODULE_LICENSE("GPL");
 
